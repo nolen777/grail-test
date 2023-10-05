0466e3047899eb4584dd1d87ff386e050399f87e
Dan Crosby <dan@danielcrosby.net>
2023-10-05 06:47:22 -0700

    use the cc_proto_library

--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -3,7 +3,11 @@
 //
 
 #include "print_hello.hpp"
+#include "src/main/protobuf/test.pb.h"
 
 int main(int argc, char* argv[]) {
   print_hello();
+
+  auto msg = new grailtest::proto::TestProto();
+  msg->set_dummy_field(15);
 }
